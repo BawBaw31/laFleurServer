769c07bc00ee7ca5e884d672db95a70671d91a11
Arthur Isnard <artisnard@gmail.com>
2022-01-10 09:37:57 +0100

    fix: change 403 as 404 error

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,14 +1,11 @@
 const validateKey = (req, res, next) => {
     try{
-        // Next security step : recongnize our app host
-        // console.log(req.headers.host);
-
         const apiKey = req.header('api-key');
 
         if (apiKey == process.env.API_KEY) {
             next();
         } else {
-            return res.status(403).json({msg: 'YOU SHALL NOT PASS !'});
+            return res.status(404).json({msg: 'YOU SHALL NOT PASS !'});
         }
 
     } catch (error) {
