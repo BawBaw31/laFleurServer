8a56fce9070947e7427d46a89c21fe5f8ab0dea6
Arthur Isnard <artisnard@gmail.com>
2022-02-02 13:36:14 +0100

    fix: sanitize req.body to prevent injections

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,8 +1,14 @@
+var sanitize = require('mongo-sanitize');
+
+
 const validateKey = (req, res, next) => {
     try{
         const apiKey = req.header('api-key');
-
+        
         if (apiKey == process.env.API_KEY) {
+            if (JSON.stringify(req.body) !== '{}') {
+                req.body = sanitize(req.body);
+            }
             next();
         } else {
             return res.status(404).json({msg: 'YOU SHALL NOT PASS !'});
